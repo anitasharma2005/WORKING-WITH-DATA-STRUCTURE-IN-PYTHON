2bcc3344e1dfe7d6ae36dc8684d5d0e6cff0bf22
anitasharma2005 <160424288+anitasharma2005@users.noreply.github.com>
2024-02-19 15:49:02 +0530

    Add files via upload

---
+++ WORKING WITH DATA STRUCTURE IN PYTHON.py
@@ -0,0 +1,10 @@
+WHOLE NUMBERS = {0, 1, 2, 3, 4, 5, 6, 8}
+NATURAL NUMBERS = {2, 4, 6, 8}
+union_result = WHOLE NUMBERS.union(NATURAL NUMBERS)
+intersection_result = WHOLE NUMBERS.intersection(NATURAL NUMBERS)
+difference_result = WHOLE NUMBERS.difference(NATURAL NUMBERS)
+symmetric_difference_result = WHOLE NUMBERS.symmetric_difference(NATURAL NUMBERS)
+print(f"Union of WHOLE NUMBERS and NATURAL NUMBERS is {union_result}")
+print(f"Intersection of WHOLE NUMBERS and NATURAL NUMBERS is {intersection_result}")
+print(f"Difference of WHOLE NUMBERS and NATURAL NUMBERS is {difference_result}")
+print(f"Symmetric difference of WHOLE NUMBERS and NATURAL NUMBERS is {symmetric_difference_result}")
